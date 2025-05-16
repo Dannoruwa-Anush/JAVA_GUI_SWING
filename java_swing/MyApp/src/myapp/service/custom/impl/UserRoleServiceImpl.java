1c0a8f020df1831442765fce3086d17bf97a8074
Dannoruwa-Anush <dannoruwalakshitha@gmail.com>
2025-05-16 17:28:34 +0530

    log is added

--- java_swing/MyApp/src/myapp/service/custom/impl/UserRoleServiceImpl.java
+++ java_swing/MyApp/src/myapp/service/custom/impl/UserRoleServiceImpl.java
@@ -6,6 +6,8 @@ package myapp.service.custom.impl;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import myapp.dao.DaoFactory;
 import myapp.dao.custom.IUserRoleDao;
 import myapp.dto.userRole.UserRoleDTO;
@@ -19,32 +21,39 @@ import myapp.service.custom.IUserRoleService;
  */
 public class UserRoleServiceImpl implements IUserRoleService{
     
+    //Log : for update, delete
+    private static final Logger LOGGER = Logger.getLogger(UserRoleServiceImpl.class.getName());
+
     IUserRoleDao userRoleDao = (IUserRoleDao) DaoFactory.getDaoFactory().getDao(DaoFactory.DaoTypes.USER_ROLE);
-    
+
     @Override
     public String addUserRole(UserRoleSaveDTO userRoleSaveDTO) throws Exception {
-        if(userRoleDao.save(new UserRoleEntity(0, userRoleSaveDTO.getRole_name()))){
+        boolean success = userRoleDao.save(new UserRoleEntity(0, userRoleSaveDTO.getRole_name()));
+        if (success) {
             return "Successfully Added";
-        }
-        else{
+        } else {
             return "Fail";
         }
     }
 
     @Override
     public String updateUserRole(UserRoleDTO userRoleDTO) throws Exception {
-        if(userRoleDao.update(new UserRoleEntity(userRoleDTO.getRole_id(), userRoleDTO.getRole_name()))){
+        if (userRoleDao.update(new UserRoleEntity(userRoleDTO.getRole_id(), userRoleDTO.getRole_name()))) {
+            LOGGER.log(Level.INFO, "User role with ID {0} updated successfully", userRoleDTO.getRole_id());
             return "Successfully Updated";
-        } else{
+        } else {
+            LOGGER.log(Level.INFO, "Failed to update user role with ID {0}", userRoleDTO.getRole_id());
             return "Fail";
         }
     }
 
     @Override
     public String deleteUserRole(Integer id) throws Exception {
-        if(userRoleDao.delete(id)){
+        if (userRoleDao.delete(id)) {
+            LOGGER.log(Level.INFO, "User role with ID {0} deleted successfully", id);
             return "Successfully Deleted";
-        } else{
+        } else {
+            LOGGER.log(Level.INFO, "Failed to delete user role with ID {0}", id);
             return "Fail";
         }
     }
@@ -52,18 +61,20 @@ public class UserRoleServiceImpl implements IUserRoleService{
     @Override
     public UserRoleDTO getUserRoleById(Integer id) throws Exception {
         UserRoleEntity userRoleEntity = userRoleDao.getById(id);
-        return new UserRoleDTO(userRoleEntity.getRole_id(), userRoleEntity.getRole_name());
+        if (userRoleEntity != null) {
+            return new UserRoleDTO(userRoleEntity.getRole_id(), userRoleEntity.getRole_name());
+        } else {
+            return null;
+        }
     }
 
     @Override
     public List<UserRoleDTO> getAllUserRoles() throws Exception {
         List<UserRoleDTO> dtos = new ArrayList<>();
-        List<UserRoleEntity> customerEntitys = userRoleDao.getAll();
-        for (UserRoleEntity customerEntity : customerEntitys) {
-            dtos.add(new UserRoleDTO(customerEntity.getRole_id(), customerEntity.getRole_name()));
+        List<UserRoleEntity> userRoleEntities = userRoleDao.getAll();
+        for (UserRoleEntity entity : userRoleEntities) {
+            dtos.add(new UserRoleDTO(entity.getRole_id(), entity.getRole_name()));
         }
-        
         return dtos;
     }
-    
 }
